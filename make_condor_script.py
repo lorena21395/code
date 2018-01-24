fd3eab33d28207485a06ee225439663855c0f6eb
Lorena Mezini <lmezini@astro0010.rcf.bnl.gov>
2018-01-24 12:11:59 -0500

    removed no deblend script

--- make_condor_script.py
+++ make_condor_script.py
@@ -12,6 +12,6 @@ print('+Experiment     = "astro"')
 
 for i in range(1,201):
     seed = np.random.randint(0, 2**15)
-    print('+job_name = "run005-'+str(i).zfill(6)+'"')
-    print('Arguments = /gpfs01/astro/workarea/lmezini/scarlet-tests/run005/run005-output-'+str(i).zfill(6)+'.fits 5000 '+str(seed)+' yes')
+    print('+job_name = "run006-'+str(i).zfill(6)+'"')
+    print('Arguments = /gpfs01/astro/workarea/lmezini/scarlet-tests/run006/run006-output-'+str(i).zfill(6)+'.fits 5000 '+str(seed)+' no')
     print('Queue')
--- shear_analyze.py
+++ shear_analyze.py
@@ -10,7 +10,7 @@ import esutil as eu
 #args = parser.parse_args()
 
 # this gets a list of all files that match the pattern
-flist = glob('/gpfs01/astro/workarea/lmezini/scarlet-tests/run004/run004*.fits')
+flist = glob('/gpfs01/astro/workarea/lmezini/scarlet-tests/run005/run005*.fits')
 
 # read each file and combine into one big array
 data = eu.io.read(flist)
