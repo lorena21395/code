56e494c458f0ba293f588476826f44666c0a6ac6
lorena21395 <lorena21395@gmail.com>
2018-02-26 08:08:33 -0500

    updating repo

--- make_condor_script.py
+++ make_condor_script.py
@@ -2,7 +2,7 @@ import numpy as np
 print('Universe        = vanilla')
 print('Notification    = Never')
 print('# Run this exe with these args')
-print('Executable      = /gpfs01/astro/workarea/lmezini/code/galsim_deblend.py')
+print('Executable      = /gpfs01/astro/workarea/lmezini/deblender_tests/galsim_deblend_2.py')
 print('Image_Size       =  1000000')
 print('GetEnv = True')
 print('kill_sig        = SIGINT')
@@ -10,8 +10,8 @@ print('#requirements = (cpu_experiment == "star") || (cpu_experiment == "phenix"
 print('requirements = (cpu_experiment == "phenix")')
 print('+Experiment     = "astro"')
 
-for i in range(1,101):
+for i in range(1,201):
     seed = np.random.randint(0, 2**15)
-    print('+job_name = "run027-'+str(i).zfill(6)+'"')
-    print('Arguments = /gpfs01/astro/workarea/lmezini/scarlet-tests/run027/run027-output-'+str(i).zfill(6)+'.fits 100 '+str(seed)+' scarlet')
+    print('+job_name = "run028-'+str(i).zfill(6)+'"')
+    print('Arguments = /gpfs01/astro/workarea/lmezini/scarlet-tests/run028/run028-output-'+str(i).zfill(6)+'.fits 100 '+str(seed)+' scarlet')
     print('Queue')
--- noise_std_analyze.py
+++ noise_std_analyze.py
@@ -6,7 +6,7 @@ import esutil as eu
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 
-flist = glob('/gpfs01/astro/workarea/lmezini/scarlet-tests/run026/run026*.fits')
+flist = glob('/gpfs01/astro/workarea/lmezini/scarlet-tests/run028/run028*.fits')
 #flist = glob('/gpfs01/astro/workarea/lmezini/code/code/test*.fits')
 #flist = glob('/gpfs01/astro/workarea/lmezini/code/test.fits')
 # read each file and combine into one big array
@@ -14,7 +14,6 @@ neigh_data = np.zeros((8,8))
 cen_data = np.zeros((8,8))
 data = eu.io.read(flist)
 for i in range(len(flist)):
-    print(data['neigh_pix_noise_std'][i])
     neigh_data +=data['neigh_pix_noise_std'][i]
     cen_data +=data['cen_pix_noise_std'][i]
 
@@ -22,8 +21,14 @@ neigh_avg = neigh_data/len(flist)
 cen_avg= cen_data/len(flist)
 print("neigh avg standard devs: ",neigh_avg)
 print("cen avg standard devs: ",cen_avg)
-plt.imshow(neigh_avg,interpolation='nearest', cmap='gray',vmin = np.min(avg),vmax =np.max(avg)) 
+plt.imshow(neigh_avg,interpolation='nearest', cmap='gray',vmin = np.min(neigh_avg),vmax =np.max(neigh_avg)) 
 plt.colorbar()
-plt.title("Avg Noise STD Bgrms=10")
-plt.savefig("test.png")
+plt.title("Neigh AvgNoise STD Bgrms=.001")
+plt.savefig("neigh_noise_std_bg001_2.png")
+plt.close()
+
+plt.imshow(cen_avg,interpolation='nearest', cmap='gray',vmin = np.min(cen_avg),vmax=np.max(cen_avg))
+plt.colorbar()
+plt.title("Cen AvgNoise STD Bgrms=.001")
+plt.savefig("cen_noise_std_bg001_2.png")
 #fitsio.write("avg_noise_std_neigh_bgrms10.fits", avg, clobber=True) 
